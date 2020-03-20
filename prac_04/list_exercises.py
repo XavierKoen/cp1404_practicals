b2e2c3efa6a4c2f26b4fa977805cb81f5dcae40a
XavierKoen <xavier.koen@my.jcu.edu.au>
2020-03-20 21:42:46 +1000

    Add username section of code to list exercises program.

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -1,8 +1,9 @@
 """
-User inputs 5 numbers, then the first number, last number,
-smallest number, largest number and the average of the 5 numbers are displayed.
+Section 1 and 2 of Intermediate Exercises.
 """
 
+# User inputs 5 numbers, then the first number, last number,
+# smallest number, largest number and the average of the 5 numbers are displayed.
 NUMBER_OF_NUMBERS = 5
 number = []
 for i in range(1, NUMBER_OF_NUMBERS + 1):
@@ -13,3 +14,12 @@ print("The smallest number is {}".format(min(number)))
 print("The largest number is {}".format(max(number)))
 total = sum(number)
 print("The average of the numbers is {:.2f}".format(total/NUMBER_OF_NUMBERS))
+
+
+# Prompt user to enter username and check if username is valid.
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn', 'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+input_name = input("Please enter username: ")
+if input_name in usernames:
+    print("Access granted")
+else:
+    print("Access denied")
