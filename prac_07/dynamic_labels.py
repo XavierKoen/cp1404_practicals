dfbe2d76bf7bd4d4d71803a7f0110dde4658d545
XavierKoen <xavier.koen@my.jcu.edu.au>
2020-05-03 13:16:38 +1000

    Add dynamic labels python and kivy files.

---
+++ prac_07/dynamic_labels.py
@@ -0,0 +1,35 @@
+"""
+Dynamically create labels based on names list
+Xavier Koen
+"""
+
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.properties import StringProperty
+from kivy.uix.label import Label
+
+
+class DynamicLabelsApp(App):
+    """Main program - Kivy app to create dynamic labels."""
+    status_text = StringProperty()
+
+    def __init__(self, **kwargs):
+        """Construct main app."""
+        super().__init__(**kwargs)
+        self.names = ['Pac-Man', 'Inky', 'Pinky', 'Blinky', 'Clyde']
+
+    def build(self):
+        """Build the Kivy GUI."""
+        self.title = "Dynamic Labels"
+        self.root = Builder.load_file('dynamic_labels.kv')
+        self.create_labels()
+        return self.root
+
+    def create_labels(self):
+        """Create labels from name list."""
+        for name in self.names:
+            temp_label = Label(text=name, id=name)
+            self.root.ids.entries_box.add_widget(temp_label)
+
+
+DynamicLabelsApp().run()
