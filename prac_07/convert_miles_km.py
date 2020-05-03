0727260c2573bc96451b9d3ed92798c712121dc1
XavierKoen <xavier.koen@my.jcu.edu.au>
2020-05-03 11:28:21 +1000

    Add error handling of invalid inputs to convert miles to kilometres python file and remove necessity for 'convert' button.

--- prac_07/convert_miles_km.py
+++ prac_07/convert_miles_km.py
@@ -10,6 +10,8 @@ from kivy.core.window import Window
 
 __author__ = 'Xavier Koen'
 
+MILES_TO_KM_CONVERSION_RATE = 1.60934
+
 
 class ConvertMilesToKilometresApp(App):
     """ ConvertMilesToKilometresApp is a Kivy App for converting miles to kilometres """
@@ -22,13 +24,20 @@ class ConvertMilesToKilometresApp(App):
 
     def handle_calculate(self, value):
         """ handle calculation (could be button press or other call), output result to label widget """
-        result = value * 1.60934
-        self.root.ids.output_label.text = str(result)
+        try:
+            result = float(value) * MILES_TO_KM_CONVERSION_RATE
+            self.root.ids.output_label.text = str(result)
+        except ValueError:
+            self.root.ids.output_label.text = '0.0'
 
     def handle_increment(self, current_input, increment):
         """ handle input change (button press of other call) to increase or decrease in text input widget value """
-        result = current_input + increment
-        self.root.ids.input_number.text = str(result)
+        try:
+            result = float(current_input) + increment
+            self.root.ids.input_number.text = str(result)
+        except ValueError:
+            result = 0 + increment
+            self.root.ids.input_number.text = str(result)
 
 
 ConvertMilesToKilometresApp().run()
