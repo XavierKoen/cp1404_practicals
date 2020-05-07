d8137b0512494ef3489ae535a86a2a3ae0ff2f6f
XavierKoen <xavier.koen@my.jcu.edu.au>
2020-05-07 22:45:28 +1000

    Modify taxi test client code to fit modified Taxi class.

--- prac_08/taxi_test.py
+++ prac_08/taxi_test.py
@@ -3,7 +3,7 @@ Code to test Taxi class.
 """
 from prac_08.taxi import Taxi
 
-prius = Taxi("Prius 1", 100, 1.23)
+prius = Taxi("Prius 1", 100)
 prius.drive(40)
 print(prius, prius.get_fare())
 prius.start_fare()
