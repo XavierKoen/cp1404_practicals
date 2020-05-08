703295f10011ba304425d75fff396cbf2463a9ad
XavierKoen <xavier.koen@my.jcu.edu.au>
2020-05-08 15:18:29 +1000

    Add flagfall to SilverServiceTaxi class and add client code to test SilverServiceTaxi class.

--- prac_08/silver_service_taxi.py
+++ prac_08/silver_service_taxi.py
@@ -12,3 +12,8 @@ class SilverServiceTaxi(Taxi):
         """Initialise a SilverServiceTaxi instance, based on parent class Taxi."""
         Taxi.__init__(self, name, fuel)
         self.price_per_km = Taxi.price_per_km * fanciness
+
+    def get_fare(self):
+        """Get fare like parent Taxi but include flagfall cost."""
+        fare = Taxi.get_fare(self) + self.flagfall
+        return fare
---
+++ prac_08/silver_service_taxi_test.py
@@ -0,0 +1,10 @@
+"""
+Client code to test SilverServiceTaxi class.
+"""
+from prac_08.silver_service_taxi import SilverServiceTaxi
+
+
+my_nice_taxi = SilverServiceTaxi("Gold-Mobile", 100, 2)
+print("Initial cost/flagfall should be $4.50, actually is ${}".format(my_nice_taxi.get_fare()))
+print("Cost/flagfall after driving 18km should be $48.78, driving distance is actually {}km and fare is actually ${}"
+      .format(my_nice_taxi.drive(18), my_nice_taxi.get_fare()))
