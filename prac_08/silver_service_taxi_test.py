f38a4e393ba3f0366f14cefbf0840e1159c41f3c
XavierKoen <xavier.koen@my.jcu.edu.au>
2020-05-08 15:34:13 +1000

    Add an overridden __str__ method for SilverServiceTaxi to include flagfall and modify client code to test this method.

--- prac_08/silver_service_taxi_test.py
+++ prac_08/silver_service_taxi_test.py
@@ -8,3 +8,6 @@ my_nice_taxi = SilverServiceTaxi("Gold-Mobile", 100, 2)
 print("Initial cost/flagfall should be $4.50, actually is ${}".format(my_nice_taxi.get_fare()))
 print("Cost/flagfall after driving 18km should be $48.78, driving distance is actually {}km and fare is actually ${}"
       .format(my_nice_taxi.drive(18), my_nice_taxi.get_fare()))
+
+hummer = SilverServiceTaxi("Hummer", 200, 4)
+print(hummer)
