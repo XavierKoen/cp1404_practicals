f08e69698f1f6970f8ceb61e0b9ac58eff1575d3
XavierKoen <xavier.koen@my.jcu.edu.au>
2020-05-31 00:52:11 +1000

    Modify wiki search code to include title and url.

--- prac_10/wiki.py
+++ prac_10/wiki.py
@@ -1,10 +1,10 @@
 import wikipedia
 
-search = ' '
-while search != '':
-    try:
-        search = input("Search: ")
+try:
+    search = input("Search: ")
+    while search != '':
         web_page = wikipedia.page(search)
-        print(web_page.summary)
-    except wikipedia.exceptions.DisambiguationError as e:
-        print(e.options)
+        print("\t-{}-\n{}\nVisit page at {}".format(web_page.title, web_page.summary, web_page.url))
+        search = input("Search: ")
+except wikipedia.exceptions.DisambiguationError as e:
+    print(e.options)
